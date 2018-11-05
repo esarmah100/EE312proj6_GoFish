9d78aaf883eb5d6b4bfab4b980fbe557b45027eb
esarmah100 <esarmah100@gmail.com>
2018-11-05 17:38:53 -0600

    Deck and Shuffle
    work in progress i think

---
+++ cmake-build-debug/CMakeFiles/EE312proj6_GoFish.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/esarmah100/EE312/GitHub/EE312proj6_GoFish/card.cpp" "C:/Users/esarmah100/EE312/GitHub/EE312proj6_GoFish/cmake-build-debug/CMakeFiles/EE312proj6_GoFish.dir/card.cpp.obj"
+  "C:/Users/esarmah100/EE312/GitHub/EE312proj6_GoFish/card_demo.cpp" "C:/Users/esarmah100/EE312/GitHub/EE312proj6_GoFish/cmake-build-debug/CMakeFiles/EE312proj6_GoFish.dir/card_demo.cpp.obj"
+  "C:/Users/esarmah100/EE312/GitHub/EE312proj6_GoFish/deck.cpp" "C:/Users/esarmah100/EE312/GitHub/EE312proj6_GoFish/cmake-build-debug/CMakeFiles/EE312proj6_GoFish.dir/deck.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/EE312proj6_GoFish.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EE312proj6_GoFish.dir/card.cpp.obj"
+  "CMakeFiles/EE312proj6_GoFish.dir/card_demo.cpp.obj"
+  "CMakeFiles/EE312proj6_GoFish.dir/deck.cpp.obj"
+  "EE312proj6_GoFish.pdb"
+  "EE312proj6_GoFish.exe"
+  "EE312proj6_GoFish.exe.manifest"
+  "libEE312proj6_GoFish.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EE312proj6_GoFish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
